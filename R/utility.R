d13dc37d960eca86d3bf26543ac95a6a291f4627
takewiki <hulilei@takewiki.com.cn>
2020-06-09 22:38:47 +0800

    add log

--- R/utility.R
+++ R/utility.R
@@ -25,3 +25,22 @@ test_tsui <- function(f,id,label,f_test) {
   shinyApp(ui, server)
   
 }
+
+
+#' 写入日志信息
+#'
+#' @param app_id 程序名称 
+#' @param info 信息
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' log_it
+log_it <- function(app_id,info) {
+  #时间字符串
+  time_str <- as.character(Sys.time())
+  res <- paste(app_id,"于",time_str,":",info)
+  print(res)
+  
+}
